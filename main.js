7a804e5b127bc88b8f6416a5b9fc3144ba9a2b18
Cass <covetouscass@proton.me>
2024-08-11 01:33:30 -0600

    feat: accidental spooky shark x]
    need to figure out why setting the
    clear color to this value results in
    black model + red background

--- main.js
+++ main.js
@@ -1,48 +1,67 @@
 import * as THREE from 'three';
-import WebGL from 'three/addons/capabilities/WebGL.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-if ( !WebGL.isWebGL2Available() ) {
-
-	// Initiate function or other initializations here
-	animate();
-
-} else {
-
-	const warning = WebGL.getWebGL2ErrorMessage();
-	document.getElementById( 'canvas-container' ).appendChild( warning );
-
-}
-
+// #########################
+// ##### Web GL Checks #####
 const CANVAS_WIDTH = 800;
 const CANVAS_HEIGHT = 600;
 
+const canvasElem = document.getElementById('canvas-container');
 const scene = new THREE.Scene();
 // Camera params - fov, aspect ratio, near clipping plane, far clipping plane
 const camera = new THREE.PerspectiveCamera( 75, CANVAS_WIDTH / CANVAS_HEIGHT, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( CANVAS_WIDTH / CANVAS_HEIGHT );
-document.getElementById('canvas-container').append( renderer.domElement ); // Shouldn't need to keep a canvas-container ref alive
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvasElem
+});
+renderer.setClearColor(0xff0000, 1)
+renderer.setSize( CANVAS_WIDTH, CANVAS_HEIGHT );
+// canvasElem.appendChild( renderer.domElement ); 
+document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+// Load a glTF resource
+const loader = new GLTFLoader();
+loader.load(
+	// resource URL
+	'low_poly_blahaj_gltf/scene.gltf',
+	// called when the resource is loaded
+	function ( gltf ) {
+
+		scene.add( gltf.scene );
+
+		gltf.animations; // Array<THREE.AnimationClip>
+	  gltf.scene; // THREE.Group
+		gltf.scenes; // Array<THREE.Group>
+		gltf.cameras; // Array<THREE.Camera>
+		gltf.asset; // Object
+
+	},
+	// called while loading is progressing
+	function ( xhr ) {
+
+		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+	},
+	// called when loading has errors
+	function ( error ) {
+
+		console.log( 'An error happened' );
+
+	}
+);
 
 camera.position.z = 5;
 
 function animate() {
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
 
 	renderer.render( scene, camera );
 }
 renderer.setAnimationLoop( animate );
 
 
-// Simple ideas:
-// Spinning blahaj x]
+// // Simple ideas:
+// // Spinning blahaj x]
 
-// Complex ideas:
-// 90s themed room looking out the window with runescape on the crt
-// Social sim of moving an avatar of me around a room, isometric viewpoint, certain articles highlighted are context objects detailing my life
+// // Complex ideas:
+// // 90s themed room looking out the window with runescape on the crt
+// // Social sim of moving an avatar of me around a room, isometric viewpoint, certain articles highlighted are context objects detailing my life
