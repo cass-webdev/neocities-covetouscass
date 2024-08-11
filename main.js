cebcd244b521d881eb34b695540bf0f8fb6aa6f0
Cass <covetouscass@proton.me>
2024-08-11 01:48:06 -0600

    feat: softly lit rotating shark :]

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
 
 // #########################
 // ##### Web GL Checks #####
@@ -14,18 +15,31 @@ const camera = new THREE.PerspectiveCamera( 75, CANVAS_WIDTH / CANVAS_HEIGHT, 0.
 const renderer = new THREE.WebGLRenderer({
   canvas: canvasElem
 });
-renderer.setClearColor(0xff0000, 1)
+// renderer.setClearColor(0xFFFFFF, 1)
 renderer.setSize( CANVAS_WIDTH, CANVAS_HEIGHT );
-// canvasElem.appendChild( renderer.domElement ); 
+
 document.body.appendChild( renderer.domElement );
+// White directional light at half intensity shining from the top.
+const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.8 );
+scene.add( directionalLight );
 
 // Load a glTF resource
 const loader = new GLTFLoader();
+
+// Optional: Provide a DRACOLoader instance to decode compressed mesh data
+// ^^ Not sure if this is working anyways ^^
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath( '/examples/jsm/libs/draco/' );
+loader.setDRACOLoader( dracoLoader );
+
+var shark;
+
 loader.load(
 	// resource URL
 	'low_poly_blahaj_gltf/scene.gltf',
 	// called when the resource is loaded
 	function ( gltf ) {
+    shark = gltf.scene;
 
 		scene.add( gltf.scene );
 
@@ -45,7 +59,7 @@ loader.load(
 	// called when loading has errors
 	function ( error ) {
 
-		console.log( 'An error happened' );
+		console.log('Error occurred: ' + error);
 
 	}
 );
@@ -53,6 +67,11 @@ loader.load(
 camera.position.z = 5;
 
 function animate() {
+
+  if (shark) {
+    shark.rotation.x += 0.01;
+    shark.rotation.y += 0.01;
+  }
 
 	renderer.render( scene, camera );
 }
