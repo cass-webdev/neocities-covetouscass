d6dd2e21f57907c62efbe75ab55e26c617082059
cass <covetouscass@proton.me>
2024-08-11 18:12:52 -0600

    feat: various improvements :]
    organized three.js code into
    functions so it's at least a little
    more clear to read
    added some extra layout stuff
    to index html
    added some more styles

--- main.js
+++ main.js
@@ -1,65 +1,38 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-// #########################
-// ##### Web GL Checks #####
-const CANVAS_WIDTH = 800;
-const CANVAS_HEIGHT = 600;
+const CANVAS_WIDTH = 600;
+const CANVAS_HEIGHT = 400;
+let scene, renderer, camera, canvasElem, shark;
 
-const canvasElem = document.getElementById('canvas-container');
-const scene = new THREE.Scene();
-// Camera params - fov, aspect ratio, near clipping plane, far clipping plane
-const camera = new THREE.PerspectiveCamera( 75, CANVAS_WIDTH / CANVAS_HEIGHT, 0.1, 1000 );
+function setupScene() {
+	canvasElem = document.getElementById('canvas-container');
+	scene = new THREE.Scene();
+	// Camera params - fov, aspect ratio, near clipping plane, far clipping plane
+	camera = new THREE.PerspectiveCamera( 75, CANVAS_WIDTH / CANVAS_HEIGHT, 0.1, 1000 );
+	camera.position.z = 5;
 
-const renderer = new THREE.WebGLRenderer({
-  canvas: canvasElem
-});
-// renderer.setClearColor(0xFFFFFF, 1)
-renderer.setSize( CANVAS_WIDTH, CANVAS_HEIGHT );
+	renderer = new THREE.WebGLRenderer({
+		canvas: canvasElem
+	});
+	renderer.setSize( CANVAS_WIDTH, CANVAS_HEIGHT );
+	canvasElem.innerHTML = renderer.domElement; // need to attach canvas to specific container
+}
 
-document.body.appendChild( renderer.domElement );
-// White directional light at half intensity shining from the top.
-const directionalLight = new THREE.DirectionalLight( 0xffffff, 1.0 );
-scene.add( directionalLight );
+function addLighting() {
+	// White directional light at half intensity shining from the top.
+	const directionalLight = new THREE.DirectionalLight( 0xffffff, 1.0 );
+	scene.add( directionalLight );
+}
 
-// Load a glTF resource
-const loader = new GLTFLoader();
-var shark;
-
-loader.load(
-	// resource URL
-	'assets/shark/scene.gltf',
-	// called when the resource is loaded
-	function ( gltf ) {
-    shark = gltf.scene;
-
-		scene.add( gltf.scene );
-
-		gltf.animations; // Array<THREE.AnimationClip>
-	  gltf.scene; // THREE.Group
-		gltf.scenes; // Array<THREE.Group>
-		gltf.cameras; // Array<THREE.Camera>
-		gltf.asset; // Object
-
-	},
-	// called while loading is progressing
-	function ( xhr ) {
-
-		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-
-	},
-	// called when loading has errors
-	function ( error ) {
-
-		console.log('Error occurred: ' + error);
-
-	}
-);
-
-camera.position.z = 5;
+async function gltfLoader() {
+	const loader = new GLTFLoader();
+	const gltfData = await modelLoader('assets/shark/scene.gltf', loader);
+	shark = gltfData.scene;
+	scene.add(shark);
+}
 
 function animate() {
-
   if (shark) {
     shark.rotation.x += 0.01;
     shark.rotation.y += 0.01;
@@ -67,8 +40,25 @@ function animate() {
 
 	renderer.render( scene, camera );
 }
-renderer.setAnimationLoop( animate );
 
+async function init() {
+	setupScene();
+	addLighting();
+	await gltfLoader();
+
+	renderer.setAnimationLoop( animate );
+}
+
+init();
+
+// Lifted from https://discourse.threejs.org/t/most-simple-way-to-wait-loading-in-gltf-loader/13896/2
+// this utility function allows you to use any three.js
+// loader with promises and async/await
+function modelLoader(url, loader) {
+  return new Promise((resolve, reject) => {
+    loader.load(url, data=> resolve(data), null, reject);
+  });
+}
 
 // Simple ideas:
 // [x] Spinning blahaj x]
